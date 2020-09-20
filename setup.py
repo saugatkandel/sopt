b1251f8bd9d4ffd54aa5953cfe225b57b5d90564
saugatkandel <32494640+saugatkandel@users.noreply.github.com>
2020-09-19 23:00:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='sopt',
-    version='0.0.1',
+    version='v0.1-beta.1',
     author='Saugat Kandel',
     author_email='saugat.kandel@u.northwestern.edu',
     packages=['sopt'],
     #packages=['optimizers', 'tests', 'benchmarks', 'examples'],
     #package_data={'data': ['*.png']},
     scripts=[],
-    description='Some second order methods for phase retrieval',
+    description='Some second order optimization methods for AD-based optimization',
     requires=[
         "numpy",
         "scipy",
